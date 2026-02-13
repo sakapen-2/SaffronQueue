f54e87b416ce7665615f56c3495febca7bfd1d6e
mmdbgr <mmdbghrsalari@gmail.com>
2026-02-13 19:20:10 +0000

    Keep only helm/saffronqueue directory

--- worker/db.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, DeclarativeBase
-from settings import settings
-
-engine = create_engine(settings.database_url, pool_pre_ping=True)
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
-
-class Base(DeclarativeBase):
-    pass
--- worker/redis_client.py
+++
@@ -1,5 +0,0 @@
-import redis
-from settings import settings
-
-def get_redis() -> redis.Redis:
-    return redis.Redis.from_url(settings.redis_url, decode_responses=True)
